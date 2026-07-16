c148caba312a514e36a37323601673b2423f1f5e
rdf453 <rtdavid05@naver.com>
2026-07-16 04:54:50 +0000

    Update enchant table implementation

---
+++ src/main/java/dev/rdf453/ApothicAutoEnchant/table/AutoEnchantingTableBlock.java
@@ -0,0 +1,34 @@
+package dev.rdf453.ApothicAutoEnchant.table;
+
+//TODO:블럭의 속성 설정
+
+import dev.shadowsoffire.apothic_enchanting.table.*;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.Nameable;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EnchantingTableBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.EnchantingTableBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.transfer.ResourceHandler;
+import net.neoforged.neoforge.transfer.item.ItemResource;
+
+public class AutoEnchantingTableBlock extends ApothEnchantingTableBlock {
+
+    public AutoEnchantingTableBlock(Block.Properties prop) {
+        super(prop);
+    }
+
+    
+}
---
+++ src/main/java/dev/rdf453/ApothicAutoEnchant/table/EnchantmentItemHandler.java
@@ -0,0 +1,30 @@
+package dev.rdf453.ApothicAutoEnchant.table;
+
+
+
+import dev.shadowsoffire.apothic_enchanting.table.EnchantmentTableItemHandler;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.transfer.item.ItemResource;
+import net.neoforged.neoforge.transfer.item.ItemStackResourceHandler;
+
+public class EnchantmentItemHandler extends ItemStackResourceHandler {
+    
+    public static final AttachmentType<EnchantmentItemHandler> TYPE = AttachmentType.serializable(EnchantmentItemHandler::new).build();
+
+    public EnchantmentItemHandler() {
+        super(2);
+    }
+
+    @Override
+    public boolean isValid(int index, ItemResource resource) {
+        if(index == 0)
+        return   resource.is(Tags.Items.ENCHANTING_FUELS);
+
+        else if(index == 1)
+        return resource.is(net.minecraft.world.item.Items.Book) || resource.is(net.minecraft.world.item.Items.ENCHANTED_BOOK);
+
+        else return false;
+    }
+}
