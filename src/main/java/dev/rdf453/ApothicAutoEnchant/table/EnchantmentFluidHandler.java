d915997daa5b805ffc61c70fb8578805326b28eb
rdf453 <rtdavid05@naver.com>
2026-09-02 07:23:13 +0000

    경험치 주입 지원하려다 애로사항 발견으로 무산

---
+++ src/main/java/dev/rdf453/ApothicAutoEnchant/table/EnchantmentFluidHandler.java
@@ -0,0 +1,20 @@
+//package dev.rdf453.ApothicAutoEnchant.table;
+
+//import net.neoforged.neoforge.transfer.fluid.FluidStacksResourceHandler;
+//import net.neoforged.neoforge.fluids.FluidStack;
+//import net.neoforged.neoforge.transfer.fluid.FluidResource;
+
+
+//xp 주입 지원
+//public class EnchantmentFluidHandler extends FluidStacksResourceHandler {
+//
+//    public EnchantmentFluidHandler() {
+//        super(1);
+//    }
+//
+//    @Override
+//    public boolean matches(FluidStack stack, FluidResource resource) {
+//        return  resource.is(Fluid.)
+//    }
+//}
+//경험치 액체 모드마다 달라서 통일할거 아니면 안하는게 나을듯
