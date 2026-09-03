8cfa77f39b633804b28d5ee4dbb9824bd4db5367
rdf453 <rtdavid05@naver.com>
2026-09-03 05:59:16 +0000

    ㄴㅇㅅ

--- src/main/java/dev/rdf453/ApothicAutoEnchant/table/TableSnapShot.java
+++
@@ -1,8 +0,0 @@
-package dev.rdf453.ApothicAutoEnchant.table;
-
-
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-import net.minecraft.core.Holder;
-import net.neoforged.neoforge.transfer.item.ItemResource;
-
-public record TableSnapShot(Object2IntOpenHashMap<Holder<ItemResource>> IO, Object2IntOpenHashMap<Holder<ItemResource>> Fuel) {}
